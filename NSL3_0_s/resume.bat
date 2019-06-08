d2ec00cf39a607a0c856eea4061f5b1505b46d36
Jacknet <Jacknet@users.noreply.github.com>
2019-06-08 17:23:54 -0400

    more resume.bat adjustments

--- NSL3_0_s/resume.bat
+++ NSL3_0_s/resume.bat
@@ -3,7 +3,8 @@
 echo Initializing environment variables
 echo For safety reasons, we cannot modify PATH for you.
 echo Ensure to add the following to PATH manually after this is done:
-echo %JAVA_HOME%\bin;%NSLJ_ROOT%\nslj\bin;%NSLJ_ROOT%\nslc\bin"
+echo "%JAVA_HOME%\bin;%NSLJ_ROOT%\nslj\bin;%NSLJ_ROOT%\nslc\bin"
+echo.
 
 setx NSLJ_ROOT as "C:\Program Files\NSL3_0_s" /m
 setx JAVA_HOME "C:\Program Files\Java\<jdk-version>" /m
@@ -12,10 +13,11 @@ setx NSL_SIM "%NSLJ_ROOT%\nslj\src" /m
 setx CLASSPATH ".;%NSLJ_ROOT%\nslc\src;%NSLJ_ROOT%;%NSL_SIM%\main;%NSL_SIM%\nsls\jacl;%NSL_SIM%\nsls\tcljava" /m
 doskey /insert
 
+echo.
 echo Compiling NSL (May produce a ton of errors)
 
 cd %NSLJ_ROOT%
-find -name "*.java" > sources.txt
+dir *.java /s /b > sources.txt
 javac -cp "%CLASSPATH%" @sources.txt
 
 pause
