c52ce6ec093e385aa71594e412c5fcde51238ec5
Jacknet <Jacknet@users.noreply.github.com>
2019-06-08 16:46:04 -0400

    fixed environment val set cmd's

--- NSL3_0_s/resume.bat
+++ NSL3_0_s/resume.bat
@@ -7,13 +7,13 @@ echo Initializing NSL environment variables
 
 set "NSLJ_ROOT=C:\Program Files\NSL3_0_s"
 set "JAVA_HOME=C:\Program Files\Java\<jdk-version>"
-set NSL_OS=nt
+set "NSL_OS=nt"
 
 echo Updating path and classpath
 
-set NSL_SIM=%NSLJ_ROOT%\nslj\src
-set PATH=%JAVA_HOME%\bin;%NSLJ_ROOT%\nslj\bin;%NSLJ_ROOT%\nslc\bin;%PATH%
-set CLASSPATH=.;%NSLJ_ROOT%\nslc\src;%NSLJ_ROOT%;%NSL_SIM%\main;%NSL_SIM%\nsls\jacl;%NSL_SIM%\nsls\tcljava
+set "NSL_SIM=%NSLJ_ROOT%\nslj\src"
+set "PATH=%JAVA_HOME%\bin;%NSLJ_ROOT%\nslj\bin;%NSLJ_ROOT%\nslc\bin;%PATH%"
+set "CLASSPATH=.;%NSLJ_ROOT%\nslc\src;%NSLJ_ROOT%;%NSL_SIM%\main;%NSL_SIM%\nsls\jacl;%NSL_SIM%\nsls\tcljava"
 doskey /insert
 
 echo Compiling NSL (May produce a ton of errors; they could be disregarded)
