9ec455340851881fa26429c46296b2c8f4814da2
Jacknet <Jacknet@users.noreply.github.com>
2019-06-08 17:18:52 -0400

    made resume.bat less dangerous

--- NSL3_0_s/resume.bat
+++ NSL3_0_s/resume.bat
@@ -1,28 +1,22 @@
 @echo off
 
-echo Please ensure that both the batch file sets JAVA_HOME
-echo to the path of your Java installation and that you are
-echo running this as an Administrator before continuing!
-pause
+echo Initializing environment variables
+echo For safety reasons, we cannot modify PATH for you.
+echo Ensure to add the following to PATH manually after this is done:
+echo %JAVA_HOME%\bin;%NSLJ_ROOT%\nslj\bin;%NSLJ_ROOT%\nslc\bin"
 
-echo Initializing NSL environment variables
-
-setx NSLJ_ROOT "C:\Program Files\NSL3_0_s" /m
+setx NSLJ_ROOT as "C:\Program Files\NSL3_0_s" /m
 setx JAVA_HOME "C:\Program Files\Java\<jdk-version>" /m
 setx NSL_OS "nt" /m
-
-echo Updating path and classpath
-
 setx NSL_SIM "%NSLJ_ROOT%\nslj\src" /m
-setx path "%PATH%;%JAVA_HOME%\bin;%NSLJ_ROOT%\nslj\bin;%NSLJ_ROOT%\nslc\bin" /m
 setx CLASSPATH ".;%NSLJ_ROOT%\nslc\src;%NSLJ_ROOT%;%NSL_SIM%\main;%NSL_SIM%\nsls\jacl;%NSL_SIM%\nsls\tcljava" /m
 doskey /insert
 
-echo Compiling NSL (May produce a ton of errors; they could be disregarded)
+echo Compiling NSL (May produce a ton of errors)
 
 cd %NSLJ_ROOT%
 find -name "*.java" > sources.txt
-javac @sources.txt
+javac -cp "%CLASSPATH%" @sources.txt
 
 pause
 
