9260d92f40eea081ae4d3141acd2a64f90d7b75f
Jacknet <Jacknet@users.noreply.github.com>
2019-06-08 16:57:48 -0400

    using setx for resume batch

--- NSL3_0_s/resume.bat
+++ NSL3_0_s/resume.bat
@@ -5,15 +5,15 @@ pause
 
 echo Initializing NSL environment variables
 
-set "NSLJ_ROOT=C:\Program Files\NSL3_0_s"
-set "JAVA_HOME=C:\Program Files\Java\<jdk-version>"
-set "NSL_OS=nt"
+setx NSLJ_ROOT "C:\Program Files\NSL3_0_s" /m
+setx JAVA_HOME "C:\Program Files\Java\<jdk-version>" /m
+setx NSL_OS "nt" /m
 
 echo Updating path and classpath
 
-set "NSL_SIM=%NSLJ_ROOT%\nslj\src"
-set "PATH=%JAVA_HOME%\bin;%NSLJ_ROOT%\nslj\bin;%NSLJ_ROOT%\nslc\bin;%PATH%"
-set "CLASSPATH=.;%NSLJ_ROOT%\nslc\src;%NSLJ_ROOT%;%NSL_SIM%\main;%NSL_SIM%\nsls\jacl;%NSL_SIM%\nsls\tcljava"
+setx NSL_SIM "%NSLJ_ROOT%\nslj\src" /m
+setx path "%PATH%;%JAVA_HOME%\bin;%NSLJ_ROOT%\nslj\bin;%NSLJ_ROOT%\nslc\bin" /m
+setx CLASSPATH ".;%NSLJ_ROOT%\nslc\src;%NSLJ_ROOT%;%NSL_SIM%\main;%NSL_SIM%\nsls\jacl;%NSL_SIM%\nsls\tcljava" /m
 doskey /insert
 
 echo Compiling NSL (May produce a ton of errors; they could be disregarded)
@@ -21,5 +21,7 @@ echo Compiling NSL (May produce a ton of errors; they could be disregarded)
 cd %NSLJ_ROOT%
 find -name "*.java" > sources.txt
 javac @sources.txt
+
+pause
 
 @echo on
